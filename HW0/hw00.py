73d4f645934b1294bc4cb67bf70beb17e5b3200a
rdhadda123 <rdhadda@stevens.edu>
2024-09-13 18:06:59 -0400

    Hw-00 Commit

---
+++ HW0/hw00.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def my_brand(assignment):
+    print("=*=*=*= Rishabh Dhadda =*=*=*=")
+    print("=*=*=*= Course 2024F-SSW567-WS =*=*=*= ")
+    print("=*=*=*=", assignment, "=*=*=*= ")
+    print("=*=*=*=", datetime.now() ,"=*=*=*= ")
+
+my_brand("HW 00 - Tools Setup")
+print("Hello World")
+my_brand("HW 00 - Tools Setup")
